c06150a1f42fdf79d8cab63e3860b77d7549d52b
Johnathan Reese <JohnathanReese8@gmail.com>
2022-10-31 18:35:57 -0400

    more styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-        <h1 className="text-center pt-5">About Us</h1>
+        <h1 className="about">About Us</h1>
         <h4 className="section"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         </h4>
     </div>
--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -14,6 +14,7 @@ function Navigation() {
               <img
               src={pianoImg}
               alt="Piano"
+              width="5%"
             />
             Piano Play
             </Link>
